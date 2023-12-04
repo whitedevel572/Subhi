0e4ef47c44107bada07f56b0b5244c5bcf3f32d8
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2023-12-04 18:01:48 +0530

    Chiku

--- Subhi/plugins/bot/start.py
+++ Subhi/plugins/bot/start.py
@@ -53,7 +53,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                random.choice(Sofia_PICS),
+                random.choice(Subhi_PICS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -105,7 +105,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(_)
-            random.choice(Sofia_PICS),
+            random.choice(Subhi_PICS),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -122,7 +122,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        random.choice(Sofia_PICS),
+        random.choice(Subhi_PICS),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -157,7 +157,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    random.choice(Sofia_PICS),
+                    random.choice(Subhi_PICS),
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
